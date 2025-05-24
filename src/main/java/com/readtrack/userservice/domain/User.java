edfc47aedd7af8617138188b48bfca6587f2891e
ivan280809 <ivanrobertomaneroii7@gmail.com>
2025-05-24 22:28:51 +0200

    Initial commit: user-service scaffolding

---
+++ src/main/java/com/readtrack/userservice/domain/User.java
@@ -0,0 +1,23 @@
+package com.readtrack.userservice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role;
+}
