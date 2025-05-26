11601555aa3f58668f5d3beaa8d84f7216ce9fad
ivan280809 <ivanrobertomaneroii7@gmail.com>
2025-05-26 16:09:27 +0200

    feat: add user registration validation and exception handling

--- src/main/java/com/readtrack/userservice/infrastructure/mappers/UserMapper.java
+++ src/main/java/com/readtrack/userservice/infrastructure/mappers/UserMapper.java
@@ -4,7 +4,6 @@ import com.readtrack.userservice.domain.models.User;
 import com.readtrack.userservice.infrastructure.models.UserMO;
 
 public interface UserMapper {
-
     UserMO toUserMO(User user);
     User toUser(UserMO userMO);
 }
